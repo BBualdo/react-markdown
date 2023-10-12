49e5fff9ecb33085b7b20a66a7888327d85fb6b5
Sebastian <mrseb10@gmail.com>
2023-10-12 22:00:40 +0200

    Preview Styled

--- src/App.tsx
+++ src/App.tsx
@@ -44,7 +44,7 @@ And here. | Okay. | I think we get it.
 1. Use just 1s if you want!
 1. And last but not least, let's not forget embedded images:
 
-![freeCodeCamp Logo](https://cdn.freecodecamp.org/testable-projects-fcc/images/fcc_secondary.svg)
+![React + TS](https://miro.medium.com/v2/resize:fit:800/0*CBjisl422hUyLxiG.png)
 `;
 	const [markdown, setMarkdown] = useState(initialState);
 
